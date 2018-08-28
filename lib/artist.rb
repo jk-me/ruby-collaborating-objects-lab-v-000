e1f2693547bf9d20c0322d0959da4ffad36ed09c
Jenny K <jkam17@gmail.com>
2018-08-28 02:42:34 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,9 +6,11 @@ class Artist
     @songs=[]
     @@all << self 
   end
+  
   def self.all 
     @@all 
   end
+  
   def self.find_or_create_by_name(name)
     self.all.each{|a| 
       if a.name==name 
@@ -17,9 +19,11 @@ class Artist
         Artist.new(name)
       end}
   end
+  
   def add_song(song)
     @songs << song 
   end 
+  
   def save
     self.all << self 
   end
