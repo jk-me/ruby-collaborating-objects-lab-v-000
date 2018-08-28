3e05b61f3ad7f0ce3546eee35a1922fc3dd16eb4
Jenny K <jkam17@gmail.com>
2018-08-28 04:51:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,15 +8,15 @@ class Song
     s = Song.new(x[1])
     
     
-    s.artist= s.artist_name=(x[0])
+    #s.artist= s.artist_name=(x[0])
     
-    #s.artist= Artist.find_or_create_by_name(x[0])
-    #s.artist.add_song(s)
+    s.artist= Artist.find_or_create_by_name(x[0])
+    s.artist.add_song(s)
     
     s
   end
-  def artist_name=(name)
-    a=Artist.find_or_create_by_name(name)
+  #def artist_name=(name)
+    #a=Artist.find_or_create_by_name(name)
     #self.artist.add_song(s)
-  end
+  #end
 end
