8186b1ae86bb8d611a21f25f7d2efcfa578fc96b
Jenny K <jkam17@gmail.com>
2018-08-28 02:36:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,20 @@
 class Artist 
-  attr_accessor :name 
+  attr_accessor :name :songs
+  @@artists=[]
   def initialize(name)
-    @name=name 
+    @name=name
+    @songs=[]
+    @@artists << self 
+  end 
+  def self.find_or_create_by_name(name)
+    @@artists.each{|a| 
+      if a.name==name 
+        return a 
+      else 
+        Artist.new(name)
+      end}
+  end
+  def add_song(song)
+    @songs << song 
   end 
 end 
