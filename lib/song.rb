4a68a86204a8dd0ccd0e66c3ffe1b4c90b0959dd
Jenny K <jkam17@gmail.com>
2018-08-28 04:14:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,11 +6,11 @@ class Song
   def self.new_by_filename(name)
     x = name.split(' - ')
     s = Song.new(x[1])
-    s.artist= Artist.find_or_create_by_name(x[0])
+    s.artist= s.artist_name=(x[0])
     s
   end
-  #def self.artist_name=(name)
-    #a=Artist.find_or_create_by_name(name)
-    
-  #end
+  def artist_name=(name)
+    a=Artist.find_or_create_by_name(name)
+    return a
+  end
 end
