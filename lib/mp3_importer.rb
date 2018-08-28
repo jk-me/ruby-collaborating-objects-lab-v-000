9e7511f7424793889c51e5f3b0e6021ced197f9a
Jenny K <jkam17@gmail.com>
2018-08-28 02:21:48 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,16 +1,18 @@
 
 
 class MP3Importer
-  attr_accessor :path 
+  attr_accessor :path, :file_array
   def initialize(path)
     @path=path
   end 
   
   def self.files
-    file_array=Dir.entries(self.path)
+    @file_array=Dir.entries(self.path)
+  end 
     
-    
-  def self.import 
+  def self.import
+    @file_array.each{|f| 
+      Song.new 
     
 end
     
